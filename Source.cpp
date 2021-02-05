8279dd2fdc1ad56e39acbf92851a9b3202605409
michael.jarvis <michael.jarvis@clinithink.com>
2021-02-05 16:45:43 +0000

    Finish solving challenge

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,28 @@
 #include <iostream>;
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main() {
 	std::string outputString;
-	std::ifstream MyReadFile("input.txt");
-	while (std::getline(MyReadFile, outputString)) {
-		// Output the text from the file
-		std::cout << outputString << "\n";
+	std::ifstream fileContents("input.txt");
+	std::vector <int> values;
+	int expectedSum = 2020;
+	while (std::getline(fileContents, outputString)) {
+		int lineInt;
+		lineInt = std::stoi(outputString);
+		values.push_back(lineInt);
 	}
-	std::cout << "test";
-}
+	for (int i = 0; i < values.size(); i++) {
+		std::vector <std::vector<int>> possibleSums;
+		for (int j = i + 1; j < values.size(); j++) {
+			possibleSums.push_back({ values.at(i), values.at(j), values.at(i) + values.at(j) });
+		}
+		for (int i = 0; i < possibleSums.size(); i++) {
+			std::vector <int> inputsAndSum = possibleSums.at(i);
+			if (inputsAndSum.at(2) == expectedSum) {
+				int multiplyResult = inputsAndSum.at(0) * inputsAndSum.at(1);
+				std::cout << multiplyResult;
+			}
+		}
+	}
