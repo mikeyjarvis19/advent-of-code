a3824bc5a6b982acdedab5733f47ca55fb6a6dd1
michael.jarvis <michael.jarvis@clinithink.com>
2021-02-05 14:59:47 +0000

    Intial commit, read input file and print to cout

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,13 @@
 #include <iostream>;
+#include <fstream>
+#include <string>
 
 int main() {
+	std::string outputString;
+	std::ifstream MyReadFile("input.txt");
+	while (std::getline(MyReadFile, outputString)) {
+		// Output the text from the file
+		std::cout << outputString << "\n";
+	}
 	std::cout << "test";
 }
